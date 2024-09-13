d6509e473e257794698240b4040fa8ff23840b47
KostyaTelelyuhin <142903343+KostyaTelelyuhin@users.noreply.github.com>
2024-09-13 16:53:06 +0300

    Initial commit

---
+++ task1/build/CMakeFiles/Execut.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Execut.dir/main.cpp.o"
+  "CMakeFiles/Execut.dir/main.cpp.o.d"
+  "Execut"
+  "Execut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Execut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ task1/build/CMakeFiles/Execut.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Execut.
+# This may be replaced when dependencies are built.
